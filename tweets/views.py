b414f06ad42f9c0698a288547a4b004969d69219
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-07-01 23:39:30 +0530

    jsonres and templates started

--- tweets/views.py
+++ tweets/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello World</h1>')
+    # return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'pages/home.html', context={}, status=200)
 
 
 def tweet_detail_view(request, tweet_id,  *args, **kwargs):
+
+    data = {
+        'id': tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f'<h1>Hello {tweet_id} - {obj.content}</h1>')
+        data['message'] = 'Not Found'
+        status = 404
+
+    return JsonResponse(data, status=status)
