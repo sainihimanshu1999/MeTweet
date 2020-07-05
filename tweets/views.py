768b41c418f411d90cc95b0bdaed8cb98d7a623f
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-07-05 15:29:37 +0530

    frontend and like button functionality

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
@@ -10,7 +11,8 @@ def home_view(request, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweet_list = [{'id': x.id, 'content': x.content} for x in qs]
+    tweet_list = [{'id': x.id, 'content': x.content,
+                   'likes': random.randint(0, 112)} for x in qs]
     data = {
         'isUser': False,
         'response': tweet_list
