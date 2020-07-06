26a559c908d6c2d0183fb59eb760501401dbc14a
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-07-06 20:20:46 +0530

    Homepage form done

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
 import random
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 from .forms import TweetForm
@@ -12,9 +12,12 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url != None:
+            return redirect(next_url)
         form = TweetForm()
 
     return render(request, 'components/form.html', context={'form': form})
