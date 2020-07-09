3b6a21274e0210aa2f39936d170221d5373a536a
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-07-09 21:51:39 +0530

    Error handling in form

--- tweets/views.py
+++ tweets/views.py
@@ -23,10 +23,12 @@ def tweet_create_view(request, *args, **kwargs):
         if request.is_ajax():
             # 201 == created items
             return JsonResponse(obj.serialize(), status=201)
-
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
 
     return render(request, 'components/form.html', context={'form': form})
 
