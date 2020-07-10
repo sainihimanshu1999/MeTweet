19c24987fc7ef9ef48dd2b75db6f9990e0fc6bab
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-07-10 23:05:14 +0530

    login persmissions done

--- metweet/settings.py
+++ metweet/settings.py
@@ -32,13 +32,17 @@ LOGIN_URL = '/login'
 # Application definition
 
 INSTALLED_APPS = [
-    'tweets',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'tweets',
+
+
 
 ]
 
