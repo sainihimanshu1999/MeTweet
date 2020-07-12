6c48f15f2d64e4bbbd496f5fa91f4e42962655c1
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-07-12 15:28:47 +0530

    Permission and authentication classes

--- tweets/views.py
+++ tweets/views.py
@@ -3,7 +3,9 @@ from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 from django.utils.http import is_safe_url
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Tweet
 from .forms import TweetForm
@@ -17,7 +19,9 @@ def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={}, status=200)
 
 
-@api_view(['POST'])  # http method the client need is POST
+@api_view(['POST'])
+# @authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])  # http method the client need is POST
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
