20a55a99554297e845018cc42189fb41e0e4eee6
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-07-16 13:07:47 +0530

    Like initiated

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Tweet
+from .models import Tweet, TweetLike
+
+
+class TweetLikeAdmin(amdin.TabularInline):
 
 
 class TweetAdmin(admin.ModelAdmin):
