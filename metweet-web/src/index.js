b233b90beb59751b7b5c929509e809bc447b5d42
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-11 21:53:16 +0530

    build file setted up

--- metweet-web/src/index.js
+++ metweet-web/src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {TweetsComponent} from './tweets';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const appEl = document.getElementById('root')
+if (appEl){
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    appEl
+  );
+}
+
+const tweetsEl =  document.getElementById('metweet')
+if(tweetsEl){
+  ReactDOM.render(
+    <React.StrictMode>
+      <TweetsComponent />
+    </React.StrictMode>,
+    tweetsEl
+  );
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
