71df775a6cbd01204d43e9c4c3f0be5cfcb29413
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-16 14:45:10 +0530

    csrf in react

--- metweet-web/src/lookup/components.js
+++ metweet-web/src/lookup/components.js
@@ -1,3 +1,19 @@
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
 function lookup(method, endpoint, callback, data) {
   let jsonData;
   if (data){
@@ -7,6 +23,14 @@ function lookup(method, endpoint, callback, data) {
   const url = `http://localhost:8000/api${endpoint}`
   xhr.responseType = "json"
   xhr.open(method, url)
+  const csrftoken = getCookie('csrftoken');
+  xhr.open(method, url)
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  if(csrftoken){
+    xhr.setRequestHeader('HTTP_X_REQUESTED_WITH', 'XMLHttpRequest')
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+    xhr.setRequestHeader('X-CSRFToken', csrftoken)
+  }     
   xhr.onload = function() {
     callback(xhr.response, xhr.status)
   }
