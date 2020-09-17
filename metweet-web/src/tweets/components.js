41ed2346a81d091aad31300f5685429e3e4d04ef
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-17 22:05:27 +0530

    retweeting functionality working

--- metweet-web/src/tweets/components.js
+++ metweet-web/src/tweets/components.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState}  from 'react'
 
-import {apitweetCreate ,apitweetList} from './lookup'
+import {apitweetAction, apitweetCreate ,apitweetList} from './lookup'
 
 export function TweetsComponent(props) {
     const textAreaRef = React.createRef()
@@ -69,37 +69,60 @@ export function TweetsList(props) {
 export function ActionBtn(props) {
     const {tweet, action} = props
     const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
-    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
+    //const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
     const className = props.className ? props.className : 'btn btn-primary btn-sm'
     const actionDisplay = action.display ? action.display : 'Action'
     
+    const handleActionBackendEvent = (response, status) => {
+      console.log(response,status)
+      if (status === 200){
+        setLikes(response.likes)
+        //setUserLike(true)
+      }
+      // if (action.type === 'like') {
+      //   if (userLike === true) {
+      //     // perhaps i Unlike it?
+      //     setLikes(likes - 1)
+      //     setUserLike(false)
+      //   } else {
+      //     setLikes(likes + 1)
+      //     setUserLike(true)
+      //   }
+        
+      // }
+    }
     const handleClick = (event) => {
       event.preventDefault()
-      if (action.type === 'like') {
-        if (userLike === true) {
-          // perhaps i Unlike it?
-          setLikes(likes - 1)
-          setUserLike(false)
-        } else {
-          setLikes(likes + 1)
-          setUserLike(true)
-        }
-        
-      }
+      apitweetAction(tweet.id, action.type, handleActionBackendEvent )
+      
     }
     const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
     return <button className={className} onClick={handleClick}>{display}</button>
   }
+
+export function ParentTweet(props){
+  const {tweet} = props
+  return tweet.parent ? <div className='row'>
+  <div className='col-11 mx-auto p-2 border rounded'>
+  <p className='mb-0 text-muted small'>Retweet</p>
+  <Tweet className={' '} tweet = {tweet.parent}/>
+  </div>
+  </div>:null
+}
   
+
 export function Tweet(props) {
     const {tweet} = props
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
     return <div className={className}>
-        <p>{tweet.id} - {tweet.content}</p>
+          <div>
+          <p>{tweet.id} - {tweet.content}</p>
+          <ParentTweet tweet={tweet}/>
+          </div>
         <div className='btn btn-group'>
           <ActionBtn tweet={tweet} action={{type: "like", display:"Likes"}}/>
           <ActionBtn tweet={tweet} action={{type: "unlike", display:"Unlike"}}/>
-          <ActionBtn tweet={tweet} action={{type: "retweet", display:""}}/>
+          <ActionBtn tweet={tweet} action={{type: "retweet", display:"Retweet"}}/>
         </div>
     </div>
   }
--- metweet-web/src/tweets/lookup.js
+++ metweet-web/src/tweets/lookup.js
@@ -4,6 +4,12 @@ import {backendlookup} from '../lookup'
 export function apitweetCreate(newTweet, callback){
     backendlookup("POST", "/tweets/create/", callback, {content: newTweet})
   }
+
+
+export function apitweetAction(tweetId, action, callback){
+    const data = {id: tweetId, action: action}
+    backendlookup("POST", "/tweets/action/", callback, data)
+  }
   
   
 export function apitweetList(callback) {
