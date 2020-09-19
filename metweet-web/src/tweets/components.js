ab4801c501b239e0cebf4ac4f9ec980203d128bb
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-19 19:10:19 +0530

    Cleaned up react done

--- metweet-web/src/tweets/components.js
+++ metweet-web/src/tweets/components.js
@@ -2,45 +2,17 @@ import React, {useState}  from 'react'
 import {ActionBtn} from './buttons'
 import {TweetsList} from './list'
 import {apitweetCreate ,apitweetList} from './lookup'
-
-function TweetCreate(props) {
-  const textAreaRef = React.createRef()
-  const {didTweet} = props
-    const handleBackendUpdate = (response, status) =>{
-      if (status === 201){
-        setNewTweets(tempNewTweets)
-      }else{
-        console.log(response)
-        alert('An error occured please try again')
-      }
-    }
-    const handleSubmit = (event) => {
-      event.preventDefault()
-      const newVal = textAreaRef.current.value
-      //backend api request
-      apitweetCreate(newVal, handleBackendUpdate)
-      textAreaRef.current.value = ''
-    }
-    return <div className={props.className}>
-          <form onSubmit={handleSubmit}>
-            <textarea ref={textAreaRef} required={true} className='form-control' name='tweet'>
-
-            </textarea>
-            <button type='submit' className='btn btn-primary my-3'>Tweet</button>
-        </form>
-  </div>
-}
+import {TweetCreate} from './create'
 
 
 export function TweetsComponent(props) {
-    const textAreaRef = React.createRef()
     const [newTweets, setNewTweets] = useState([])
     const canTweet = props.canTweet === false ? false : true
     //backend api response 
     const handleNewTweet = (newTweet) =>{
       let tempNewTweets = [...newTweets]
-      setNewTweets(tempNewTweets)
       tempNewTweets.unshift(newTweet)
+      setNewTweets(tempNewTweets)
     }
     return <div className={props.className}>
             {canTweet === true && <TweetCreate didTweet={handleNewTweet} className='col-12 mb-3'/>}
--- metweet-web/src/tweets/index.js
+++ metweet-web/src/tweets/index.js
@@ -2,10 +2,12 @@ import {ActionBtn} from './buttons'
 import {TweetsList} from './list'
 import {Tweet} from './detail'
 import {TweetsComponent} from './components'
+import {TweetCreate} from './create'
 
 export {
     ActionBtn, 
     Tweet,
     TweetsList,
-    TweetsComponent
+    TweetsComponent,
+    TweetCreate
 }
