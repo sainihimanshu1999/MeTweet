6cce00a3c044b45480773b94985e837885e2f28a
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-23 16:01:34 +0530

    django to react moving in the react app done

--- metweet/urls.py
+++ metweet/urls.py
@@ -17,18 +17,26 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from tweets.views import home_view, tweet_detail_view, tweet_list_view, tweet_create_view, tweet_delete_view, tweet_action_view
+from tweets.views import (
+    local_tweets_list_view,
+    local_tweets_detail_view,
+    local_tweets_profile_view,
+)
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
-    path('react/', TemplateView.as_view(template_name='react.html')),
-    path('create-tweet/', tweet_create_view),
-    path('tweets/', tweet_list_view),
-    path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('', local_tweets_list_view),
+    path('<int:tweet_id>', local_tweets_detail_view),
+    path('profile/<str:username>', local_tweets_profile_view),
+    path('api/tweets/', include('tweets.urls')),
+    #old ways of using url just in django
+    # path('react/', TemplateView.as_view(template_name='react.html')),
+    # path('create-tweet/', tweet_create_view),
+    # path('tweets/', tweet_list_view),
+    # path('tweets/<int:tweet_id>', tweet_detail_view),
     # path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
     # path('api/tweets/action', tweet_action_view),
-    path('api/tweets/', include('tweets.urls'))
+    
 
 ]
 
