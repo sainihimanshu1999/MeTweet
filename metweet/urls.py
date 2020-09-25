027a7c9bc7d0c231781bca71c9fcb496c32bf098
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-25 22:57:45 +0530

    authetication changes done for user profiles

--- metweet/urls.py
+++ metweet/urls.py
@@ -22,10 +22,18 @@ from tweets.views import (
     tweets_detail_view,
     tweets_profile_view,
 )
+from accounts.views import(
+    login_view,
+    logout_view,
+    register_view,
+)
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tweets_list_view),
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('register/', register_view),
     path('<int:tweet_id>', tweets_detail_view),
     path('profile/<str:username>', tweets_profile_view),
     path('api/tweets/', include('tweets.api.urls')),
