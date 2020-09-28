7d76bde054c6e35d288977d4b35a8f17b401a7a6
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-28 13:33:15 +0530

    django signals applied and done

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render 
-
+from django.http import Http404
+from .models import Profile
+from django.db.models.signals import post_save
 
 def profile_detail_view(request, username, *args, **kwargs):
     # get the profile for the passed username
-    return render(request, "profiles/detail.html", {'username': username})
+    qs = Profile.objects.filter(user__username = username)
+    if not qs.exists():
+        raise Http404
+    profile_obj = qs.first()
+    context = {
+        'username':username,
+        'profile': profile_obj
+    }
+    return render(request, "profiles/detail.html", context)
+
+
+
 
 
