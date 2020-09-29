d3319d8512a3f9e2b1af4f13a2aae088ec3e4153
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-29 18:10:58 +0530

    checking follower following logic done

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Profile
 
-# Create your tests here.
+User = get_user_model()
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='def', password = 'himanshu')
+        self.userb = User.objects.create_user(username='abc', password = 'himanshu')
+
+    def test_profile_created_via_signal(self):
+        qs = Profile.objects.all()
+        self.assertEqual(qs.count(), 2)
+
+    def test_following(self):
+        first = self.user
+        second = self.userb
+        first.profile.followers.add(second) #we added a follower here
+        qs = second.following.filter(user = first) #checking that second user is following first
+        self.assertTrue(qs.exists())
+        first_user_following_no_one = first.following.all()
+        self.assertFalse(first_user_following_no_one.exists())
