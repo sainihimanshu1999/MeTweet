16ba8abc356ea909a5812b574cc865c42159fa70
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-30 19:35:33 +0530

    json object serialize error

---
+++ profiles/api/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import (
+    user_follow_view,
+    )
+
+'''
+CLIENT BASE ENDPOINT /api/profile/
+'''
+urlpatterns = [
+    path('<str:username>/follow/', user_follow_view),
+]
---
+++ profiles/api/views.py
@@ -0,0 +1,51 @@
+import random
+import json
+from ..models import Profile
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+User = get_user_model()
+
+
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def user_profile_detail_view(request, username, *args, **kwargs):
+#     current_user = request.user
+#     to_follow_user = ??
+#     return Response({}, status=200)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_follow_view(request, username, *args, **kwargs):
+    me = request.user
+    other_user_qs = User.objects.filter(username = username) 
+    if not other_user_qs.exists():
+        return Response({}, status=404)
+    other = other_user_qs.first()
+    profile = other.profile
+    data = {}
+    try:
+        data = request.data
+    except:
+        pass
+    print(data)
+    action = data.get("action")
+    if action == "follow":
+        profile.followers.add(me)
+    elif action == "unfollow":
+        profile.followers.remove(me)
+    else:
+        pass
+    return Response({"followers": profile.followers.all()}, status=400)
+
+
+ 
